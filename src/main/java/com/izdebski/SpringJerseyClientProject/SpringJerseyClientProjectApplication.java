873440f734f27b645e625274bf72709274a97de5
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2021-01-19 23:38:59 +0200

    Add files via upload

---
+++ src/main/java/com/izdebski/SpringJerseyClientProject/SpringJerseyClientProjectApplication.java
@@ -0,0 +1,14 @@
+package com.izdebski.SpringJerseyClientProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJerseyClientProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJerseyClientProjectApplication.class, args);
+	}
+
+}
+
